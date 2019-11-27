b57cdbd1ef78274f6e8b46f5b247ecdbc15d9333
xfz <xfzandzjj@163.com>
2019-11-27 19:16:21 +0800

    權限校驗

--- src/main/comm/middleware/AuthorizationHandler.go
+++ src/main/comm/middleware/AuthorizationHandler.go
@@ -3,6 +3,8 @@ package middleware
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"goAdmin/src/main/service"
+	"goAdmin/src/main/utils"
 	"net/http"
 )
 
@@ -17,8 +19,32 @@ import (
 func AuthorizationHandler(permission string) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		fmt.Println("<<<<<<<<<<authorization.【" + permission + "】begin>>>>>>>>>>>>>>")
-		context.Status(http.StatusOK)
-		context.Next()
-		fmt.Println("<<<<<<<<<<authorization.【" + permission + "】end>>>>>>>>>>>>>>")
+		//解析 登入校驗後存入的用戶信息
+		claimsData, exists := context.Get("claims")
+		if !exists {
+			context.JSON(http.StatusUnauthorized, utils.Error(401, "获取不到当前用户信息.[system.get.claims.not.exist]", nil))
+			context.Abort()
+			return
+		}
+		customClaims := claimsData.(*utils.CustomClaims)
+		permsList, _ := service.FindPermsByUserId(customClaims.ID)
+		if len(permsList) <= 0 {
+			context.JSON(http.StatusUnauthorized, utils.Error(401, "用戶未授權", nil))
+			context.Abort()
+			return
+		}
+		var isPermission = false
+		for _, perm := range permsList {
+
+			if perm == permission {
+				isPermission = true
+				break
+			}
+		}
+		if !isPermission {
+			context.JSON(http.StatusUnauthorized, utils.Error(401, "用戶未授權", nil))
+			context.Abort()
+			return
+		}
 	}
 }
